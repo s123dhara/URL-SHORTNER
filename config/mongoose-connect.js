42824f0b0985feafb5359d9f7bb178f0de96a64d
s123dhara <spdh427@gmail.com>
2024-05-30 00:39:11 +0530

    atlas connection

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/URLshortnerdb")
+mongoose.connect("mongodb+srv://spdh427:wOw9EZoA4Ye47t8m@suprioy.jbdl4qj.mongodb.net/URLshortnerdb")
 .then((res)=>{
     console.log("Mongodb connected!")
 }).catch((err)=>{
